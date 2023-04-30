6c917d7714de95c23d206d88e3661b3a1652e8e6
bblinder62 <btblinder1@gmail.com>
2023-04-30 20:49:36 +0000

    created move_base and gui nodes

---
+++ src/cargo_bot.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+import math
+import tf
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from tf.transformations import euler_from_quaternion
+import math
+import os
+import signal
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+"""Good standard starting code for testing a robot"""
+
+class Robot:
+    def __init__(self):
+        self.OGSpeed=0.2 #original speed of robot
+        self.speed=self.OGSpeed #used speed of robot
+        self.turnspeed=1 #rad/sec
+        self.x=0 #x position
+        self.y=0 #y position
+        self.yaw=0 #yaw  
+        self.waypoints=[[],[]]
+
+        self.state="H"
+        self.last_key_press_time=rospy.Time.now()
+    
+    def setHome(self):
+        self.waypoints[0]=self.createWaypoint()
+        print(self.waypoints[0])
+    def setGoal(self):
+        self.waypoints[1]=self.createWaypoint()
+        print(self.waypoints[0])
+
+    def createWaypoint(self):
+        return [[self.x,self.y,0],[0,0,0,self.yaw]]
+
+    def goal_pose(self,pose):
+        goal_pose = MoveBaseGoal()
+        goal_pose.target_pose.header.frame_id = 'map'
+        goal_pose.target_pose.pose.position.x = pose[0][0]
+        goal_pose.target_pose.pose.position.y = pose[0][1]
+        goal_pose.target_pose.pose.position.z = pose[0][2]
+        goal_pose.target_pose.pose.orientation.x = pose[1][0]
+        goal_pose.target_pose.pose.orientation.y = pose[1][1]
+        goal_pose.target_pose.pose.orientation.z = pose[1][2]
+        goal_pose.target_pose.pose.orientation.w = pose[1][3]
+        return goal_pose
+
+    def clear(self): #clears output
+        os.system('clear')
+
+    def checkDist(self,x1,y1,x2,y2): #Performs a simple calculation to find the distance between the current x and y position and the start position
+        x3=(x1-x2)**2
+        y3=(y1-y2)**2
+        return math.sqrt(x3+y3)
+
+    # print the state of the robot
+    def print_state(self):
+        # print(self.state)
+        # self.clear()
+        # # calculate time since last key stroke
+        time_since = rospy.Time.now() - self.last_key_press_time
+    
+        # #print for terminal
+        # print("---")
+        # # print("Velocity: " + str(self.vel))
+        # print("Position: ("+ str(self.x)+","+ str(self.y)+")")
+        # print("SECS SINCE LAST KEY PRESS: " + str(time_since.secs))
+        # print("---")
+    
+        # #return as single string for GUI
+        return("---\nSTATE: " + self.state+"\nPosition: ( x= "+ str(self.x)+", y="+ str(self.y)+")\nSECS SINCE LAST BUTTON PRESS: " + str(time_since.secs)+"\n---")
+    
+    
+
+
+    def moveForward(self): #sets movement to forwards
+        t.linear.x = self.speed
+    def moveBack(self): #Sets movement back
+        t.linear.x = -self.speed
+    def moveHold(self): #stops movement
+        t.linear.x=0.0
+
+    def turnLeft(self): #sets turn left
+        t.angular.z = self.turnspeed
+    def turnRight(self): #sets turn left
+        t.angular.z = -self.turnspeed
+    def turnHold(self): #stops turn
+        t.angular.z = 0.0
+
+    def checkMove(self):
+        if self.state=="ff" or self.state=="bb":
+            self.speed=self.OGSpeed+0.2
+        else:
+            self.speed=self.OGSpeed
+
+        if self.state=="f":
+            self.moveForward()
+        elif self.state=="ff":
+            self.moveForward()
+        elif self.state=="b":
+            self.moveBack()
+        elif self.state=="bb":
+            self.moveBack()
+        elif self.state=="l":
+            self.turnLeft()
+        elif self.state=="r":
+            self.turnRight()
+        
+        elif self.state=="sh":
+            self.setHome()
+            self.state="h"
+        elif self.state=="gh":
+            try:
+                goal = self.goal_pose(self.waypoints[0])
+                print("Going for goal: ", goal)
+                client.send_goal(goal)
+                # client.wait_for_result()
+            except:
+                print("No Waypoint set")
+            self.state="h"
+        elif self.state=="sg":
+            self.setGoal()
+            self.state="h"
+        elif self.state=="gg":
+            try:
+                goal = self.goal_pose(self.waypoints[1])
+                print("Going for goal: ", goal)
+                client.send_goal(goal)
+                # client.wait_for_result()
+            except:
+                print("No Waypoint set")
+            self.state="h"
+        
+        elif self.state=="Auto-Move":
+            pass
+
+        elif self.state=="h":
+            self.moveHold()
+            self.turnHold()
+        else:
+            self.moveHold()
+            self.turnHold()
+        
+
+    def shutdown(self,sig, stackframe):
+        print("Exit because of ^c")
+        self.moveHold()
+        self.turnHold()
+        sys.exit(0)
+
+
+#Key press callback
+def key_cb(msg):
+    if rob.state==msg.data and (rob.state=="f" or rob.state=="b"): #keeps track of if its a double input or not
+        rob.state="" + msg.data+""+msg.data
+    else:
+        rob.state = msg.data
+    rob.last_key_press_time = rospy.Time.now()
+
+# odom data
+def odom_cb(msg):
+    rob.x=msg.pose.pose.position.x
+    rob.y=msg.pose.pose.position.y
+    quarternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+    (roll, pitch, rob.yaw) = euler_from_quaternion(quarternion)
+
+#Controls Shutdown
+def shutdown(sig, stackframe):
+    print("\nControlled Exit Due To ^C")
+    rob.moveHold()
+    rob.turnHold()
+    cmd_vel_pub.publish(t)
+    # cmd_vel_pub.publish(t)
+    sys.exit(0)
+
+
+# init node
+rospy.init_node('robot')
+rob=Robot()
+
+
+# RUN rosrun prrexamples key_publisher.py to get /keys
+key_sub = rospy.Subscriber('keys', String, key_cb) 
+odom_sub = rospy.Subscriber('odom', Odometry, odom_cb)
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+ui_pub=rospy.Publisher("UI", String, queue_size=10) #publishes to my own topic with the string for the GUI
+
+client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+client.wait_for_server()
+
+# start in state halted and grab the current time
+rob.last_key_press_time = rospy.Time.now()
+
+# set rate
+rate = rospy.Rate(10)
+
+# Wait for published topics, exit on ^c
+while not rospy.is_shutdown():
+
+   # print out the current state and time since last key press
+
+   # publish cmd_vel from here 
+    t = Twist()
+
+    rob.checkMove()
+
+    cmd_vel_pub.publish(t)
+    ui_pub.publish(rob.print_state())
+
+    signal.signal(signal.SIGINT, shutdown)
+
+   # run at 10hz
+    rate.sleep()
+    
---
+++ src/cargo_gui.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+import tkinter #this is the GUI I chose
+from std_msgs.msg import String
+import rospy
+import rosnode
+
+
+"""MAKE THE ROBOT DANCE GUI"""
+
+root=tkinter.Tk() #init window
+root.wm_title("Control Panel") #set window name
+root.geometry("500x500") #set size of window
+
+message=" "
+def ui_cb(msg): #recives string from robot node
+    global message
+    message=str(msg.data)
+    updateMessages()
+    updateTime()
+
+#creates all the text variables for displaying the robot data
+text1=tkinter.StringVar()
+text1.set("Please Start Main Node")
+text2=tkinter.StringVar()
+text2.set(" ")
+text3=tkinter.StringVar()
+text3.set(" ")
+text4=tkinter.StringVar()
+text4.set(" ")
+text5=tkinter.StringVar()
+text5.set(" ")
+
+#updates all the text for robot data
+def updateMessages():
+    # print(("/robot") in rosnode.get_node_names())
+    if message!=" ":
+        wordList=message.split("\n")
+        text1.set(wordList[0])
+        text2.set(wordList[1])
+        text3.set(wordList[2])
+        text4.set(wordList[3])
+        text5.set(wordList[4])
+    else:
+        text1.set("Please Start Main Node")
+        text2.set(" ")
+        text3.set(" ")
+        text4.set(" ")
+        text5.set(" ")
+
+def updateTime():
+    """
+    IDEA 1:
+    make a heartbeat publisher that publishes in the main loop of the other node saying that it still exists,
+    and if a certain amount of time has elapsed since GUI heartbeat_cb has run, then you display it stopped,
+    and if there is a manual exit, send that through the heartbeat topic.
+
+    IDEA 2:
+    Google if there is a way in ROS to tell the status of another node.
+    """
+    pass
+
+#All functions that are called by pressing GUI buttons
+def pubH():
+    key_pub.publish("h")
+def pubL():
+    key_pub.publish("l")
+def pubR():
+    key_pub.publish("r")
+def pubF():
+    key_pub.publish("f")
+def pubB():
+    key_pub.publish("b")
+
+def pubSH():
+    key_pub.publish("sh")
+def pubGH():
+    key_pub.publish("gh")
+def pubSG():
+    key_pub.publish("sg")
+def pubGG():
+    key_pub.publish("gg")
+# def pub(message):
+#     key_pub.publish(message)
+#     print(message)
+
+
+
+#initalizing rospy stuff 
+rospy.init_node("tkinter")
+key_pub = rospy.Publisher('keys', String, queue_size=1) #publishes to the same topic as key_publisher.py
+ui_sub = rospy.Subscriber('UI', String, ui_cb) #recives the information from the robot node in the UI topic
+
+
+#creating labels to display robot data
+label1=tkinter.Label(root,textvariable=text1, width=500)
+label1.pack(side=tkinter.TOP)
+
+label2=tkinter.Label(root,textvariable=text2, width=500)
+label2.pack(side=tkinter.TOP)
+
+label3=tkinter.Label(root,textvariable=text3, width=500)
+label3.pack(side=tkinter.TOP)
+
+label4=tkinter.Label(root,textvariable=text4, width=500)
+label4.pack(side=tkinter.TOP)
+
+label5=tkinter.Label(root,textvariable=text5, width=500)
+label5.pack(side=tkinter.TOP)
+
+# label6=tkinter.Label(root,textvariable=text6, width=500)
+# label6.pack(side=tkinter.TOP)
+
+#frames to orgranize buttons
+frame1=tkinter.Frame(
+    root
+)
+frame2=tkinter.Frame(
+    root
+)
+frame3=tkinter.Frame(
+    root
+)
+
+#buttons to press to control the robot
+forward=tkinter.Button(
+    frame2,
+    text="Forward",
+    bg="light green",
+    command=pubF
+)
+
+back=tkinter.Button(
+    frame2,
+    text="Back",
+    bg="light green",
+    command=pubB
+)
+
+left=tkinter.Button(
+    frame1,
+    text="Left",
+    bg="light green",
+    command=pubL
+)
+
+right=tkinter.Button(
+    frame3,
+    text="Right",
+    bg="light green",
+    command=pubR
+)
+
+hold=tkinter.Button(
+    frame2,
+    text="STOP",
+    bg="red",
+    command=pubH
+)
+
+SetHome=tkinter.Button(
+    frame1,
+    text="Set Home",
+    bg="grey",
+    command=pubSH
+)
+
+GoHome=tkinter.Button(
+    frame1,
+    text="Go Home",
+    bg="grey",
+    command=pubGH
+)
+
+SetGoal=tkinter.Button(
+    frame3,
+    text="Set Goal",
+    bg="grey",
+    command=pubSG
+)
+
+GoGoal=tkinter.Button( #an empty button which would have a different command but I ran out of time
+    frame3,
+    text="Go Goal",
+    bg="grey",
+    command=pubGG
+)
+
+#packing frames onto tkinter window
+frame1.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+frame2.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+frame3.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+
+#packing buttons onto the frames in a percise order so it looks right
+SetGoal.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+forward.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+SetHome.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+left.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+hold.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+right.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+GoGoal.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+back.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+GoHome.pack(side=tkinter.TOP, expand=True, fill=tkinter.BOTH)
+
+
+
+
+
+
+# rate = rospy.Rate(10)
+root.mainloop() #tktiner mainloopa
+# while  not rospy.is_shutdown(): #Replacement mainloop
+#     root.update_idletasks()
+#     root.update()
+#     rate.sleep()
